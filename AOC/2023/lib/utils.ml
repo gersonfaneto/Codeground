2b9f4d41835aaa98c61f0a12005b55b87fb8d5f0
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-12-01 23:18:20 -0300

    feat(AOC/2023): Added some utility functions under `lib/utils`.

---
+++ AOC/2023/lib/utils.ml
@@ -0,0 +1,26 @@
+let read_lines filename =
+  let channel = open_in filename in
+  let rec aux acc =
+    match input_line channel with
+    | line -> aux (line :: acc)
+    | exception End_of_file ->
+      close_in channel;
+      List.rev acc
+  in
+  aux []
+;;
+
+let reverse_string str =
+  let rec aux idx =
+    match idx with
+    | 0 -> Char.escaped str.[0]
+    | _ -> Char.escaped str.[idx] ^ aux (idx - 1)
+  in
+  aux (String.length str - 1)
+;;
+
+let is_digit char =
+  match char with
+  | '0' .. '9' -> true
+  | _ -> false
+;;
