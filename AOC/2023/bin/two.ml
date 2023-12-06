2567ecd249b24d630ce2d621ceae7a547b28a0aa
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-12-06 16:00:47 -0300

    feat(AOC/2023): Solution for Part Two of Day Two!

--- AOC/2023/bin/two.ml
+++ AOC/2023/bin/two.ml
@@ -31,3 +31,34 @@ let () =
 ;;
 
 (* Day Two - Part II *)
+
+let max_of_pairs pairs =
+  List.fold pairs ~init:1 ~f:(fun acc pair ->
+    let value = String.split pair ~on:' ' |> List.hd_exn |> String.strip in
+    let value = int_of_string value in
+    if value > acc then value else acc)
+;;
+
+let filter_by_color pairs color =
+  List.filter pairs ~f:(fun pair ->
+    let color' = String.split pair ~on:' ' |> List.last_exn |> String.strip in
+    String.equal color color')
+;;
+
+let () =
+  let lines = Utils.read_lines "inputs/02-01.txt" in
+  Printf.printf " Part I\n";
+  List.fold lines ~init:0 ~f:(fun acc line ->
+    let game = String.split line ~on:':' |> List.last_exn |> String.strip in
+    let sets = String.split game ~on:';' |> List.map ~f:String.strip in
+    let pairs =
+      List.fold sets ~init:[] ~f:(fun acc set ->
+        let pairs = String.split set ~on:',' |> List.map ~f:String.strip in
+        acc @ pairs)
+    in
+    let red_max = filter_by_color pairs "red" |> max_of_pairs in
+    let green_max = filter_by_color pairs "green" |> max_of_pairs in
+    let blue_max = filter_by_color pairs "blue" |> max_of_pairs in
+    acc + (red_max * green_max * blue_max))
+  |> Printf.printf "       Answer: %d\n"
+;;
